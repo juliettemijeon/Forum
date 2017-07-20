b7e662f9a3bce152f45e7962fb3c59567cf99172
juliette_mijeon <juliette.mijeon@nr-communication.fr>
2017-07-20 11:05:33 +0200

    ajout config

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new UsersBundle\UsersBundle(),
+            new BaseBundle\BaseBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
