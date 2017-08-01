588271f15bdfaf6dffda2f5c938d0ddd7f6183f7
juliettemijeon <juliette.mijeon@gmail.com>
2017-08-01 11:32:27 +0200

    ajout du routing

--- src/ForumBundle/Controller/CategoryController.php
+++ src/ForumBundle/Controller/CategoryController.php
@@ -3,16 +3,32 @@
 namespace ForumBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\BrowserKit\Response;
+use Forum\ForumBundle\Entity\Category;
 
 class CategoryController extends Controller
 {
+    /**
+     * @Route("/categories",name="view_categories")
+     * @method mixed $viewCategoryAction()
+     *
+     * @return Response
+     */
     public function viewCategoriesAction()
     {
+        //Aller les chercher toutes les catégories
         return $this->render('ForumBundle:Category:view_categories.html.twig', array(
             // ...
         ));
     }
 
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
     public function createCategoryAction()
     {
         return $this->render('ForumBundle:Category:create_category.html.twig', array(
@@ -20,6 +36,11 @@ class CategoryController extends Controller
         ));
     }
 
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
     public function updateCategoryAction()
     {
         return $this->render('ForumBundle:Category:update_category.html.twig', array(
